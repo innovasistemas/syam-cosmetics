f98cd2e71b4ed8f169db8356b28e9939527320ff
DESARROLLOTEC1 <DESARROLLOTEC1@syamda.local>
2020-11-04 18:05:58 -0500

    Extracción de datos: ajustes en consultas

--- envia-webservice/assets/sql/Scripts-plugin-envia.sql
+++ envia-webservice/assets/sql/Scripts-plugin-envia.sql
@@ -1,51 +1,76 @@
 use desarrollossylog_tienda;
 
+
 SELECT *
-FROM desarrollossylog_tienda.wp_posts 
+FROM wp_posts 
 WHERE post_type like '%product%'
 
+
 SELECT *
-FROM desarrollossylog_tienda.wp_posts 
+FROM wp_posts 
 WHERE post_type ='shop_order'
 
-SELECT wu.user_email 
-FROM desarrollossylog_tienda.wp_posts wp inner join wp_users wu 
+
+SELECT *
+FROM wp_posts 
+-- where post_author <> 1
+where ID = 9487 -- ---> Pedido
+-- where ID = 8079 -- ---> Producto
+
+
+SELECT wp.*
+FROM wp_posts wp inner join wp_users wu 
 	on wp.post_author = wu.ID 
-WHERE post_type = 'shop_order'
+WHERE post_type = 'shop_order' AND post_status = 'wc-cancelled';
+
+SELECT wp.*
+FROM wp_posts wp inner join wp_users wu 
+	on wp.post_author = wu.ID 
+WHERE post_status = 'wc-cancelled';
+
+
+SELECT wp.*
+FROM wp_posts wp inner join wp_users wu 
+	on wp.post_author = wu.ID 
+WHERE post_type = 'shop_order' ;
+
+
+select * from wp_users wu;
+
+
+select * from wp_usermeta wum;
+
+
+select wwopl.*, wp.ID, wp.post_title 
+from wp_posts wp inner join wp_wc_order_product_lookup wwopl 
+	on wp.ID = wwopl.product_id 
+where wwopl.order_id = 9487;
+
+
+select count(*)
+from wp_postmeta 
+
+
+select *
+from wp_postmeta 
+where post_id = 8079 -- producto
+
+
+select *
+from wp_postmeta 
+where post_id = 9487 -- pedido
+
+
+select * from wp_users wu
+
+
+select * from wp_usermeta wu2 where user_id = 23
 
 
 -- --------------------------------------------------------
 -- --------------------------------------------------------
 -- Generación guía
-select  
-	concat(dcc.codigo_ciudad, ' - ', dcc.nombre_ciudad) as 'Ciudad_Origen',
-	wpm.meta_value as 'Ciudad_Destino',
-	'4' as 'Cod_FormaPago',
-	'12' as 'Cod_Servicio',
-	'1' as 'Num_Unidades',
-	'1' as 'MPesoReal_K',
-	'1' as 'MPesoVolumen_K',
-	wpm.meta_value as 'Valor_Declarado',
-	'0' as 'Mca_DocInternacional',
-	'3' as 'Cod_Regional_Cta',
-	'1' as 'Cod_Oficina_Cta',
-	'03-001-0004582' as 'Cod_Cuenta',
-	'SYAM COSMETICS SAS' as 'Nom_Remitente',
-	'Carrera 54 # 79AA Sur - 40 Bodega 140 La Troja' as 'Dir_Remitente',
-	'5963719' as 'Tel_Remitente',
-	'901040898-6' as 'Ced_Remitente'
-from dt_conf_ciudades dcc, 
-	 wp_postmeta wpm 
-where dcc.nombre_ciudad = 'LA ESTRELLA' and 
-	  (wpm.meta_key = '_billing_city' or
-	  wpm.meta_key = '_order_total') and
-	  -- wpm.meta_id = wpm2.meta_id and
-	  wpm.post_id = '9486' 
--- group by wpm.post_id 
--- limit 1
-;
 
--- --------------------------------------------------------
 select distinct 
 	concat(dcc.codigo_ciudad, ' - ', dcc.nombre_ciudad) as 'Ciudad_Origen',
 	wpm.meta_value as 'Ciudad_Destino',
@@ -65,9 +90,24 @@ select distinct
 	'901040898-6' as 'Ced_Remitente',
 	concat(wpm.meta_value, ' - ', wpm.meta_value) as 'Nom_Destinatario',
 	'9487' as 'Num_Documentos', -- Número de pedido para solicitar la generación de la guía
-	'Dice_Contener' as 'Dice_Contener',  -- Pendiente: corresponde al detalle del pedido
-	post_excerpt as 'Texto_Guia',
-	'' as 'Accion_NotaGuia'
+	(select 
+		case wp.post_title
+			when wp.post_title like '%KIT PIEL CANELA%'
+				then concat('PG.', wwopl.product_qty, wwopl.product_id, 'kpc/')   -- Pendiente: corresponde al detalle del pedido
+			when wp.post_title like '%CARBÓN ACTIVADO WIKI%' 
+				then concat('PG.', wwopl.product_qty, wwopl.product_id, 'carb/')  -- Pendiente: corresponde al detalle del pedido
+			else
+				'x'
+		end
+	from wp_posts wp
+	where wp.ID = 9487) as 'Dice_Contener',
+	wp.post_excerpt as 'Texto_Guia',
+	'' as 'Accion_NotaGuia',
+	'' as 'Numero_Guia',
+	'' as 'CentroCosto',
+	'' as 'Con_Cartaporte',
+	'' as 'generar_os',
+	'0' as 'valorproducto'
 from 
  	dt_conf_ciudades dcc, 
  	wp_posts wp inner join
@@ -75,6 +115,10 @@ from
 		on wp.ID = wpm.post_id 
 	inner join wp_postmeta wpm2 
 		on wpm.post_id = wpm2.post_id 
+	inner join wp_wc_order_product_lookup wwopl
+		on wp.ID = wwopl.order_id 
+	inner join wp_posts wp1 	
+		on wp1.ID = wwopl.product_id 
 where 
  	dcc.nombre_ciudad = 'LA ESTRELLA' and 
 	(wpm.meta_key = '_billing_city' or
@@ -86,29 +130,26 @@ where
 -- group by wpm.meta_id 
 -- limit 1	  
 ;	  
-	  
-
-	  
+	    
 -- --------------------------------------------------------	  
 -- --------------------------------------------------------
 
+select ID, 
+	case wp.post_title
+		when wp.post_title like '%KIT PIEL CANELA%'
+			then concat('PG.', wwopl.product_qty, wwopl.product_id, 'kpc/')   -- Pendiente: corresponde al detalle del pedido
+		when wp.post_title like '%CARBÓN ACTIVADO WIKI%' 
+			then concat('PG.', wwopl.product_qty, wwopl.product_id, 'carb/')  -- Pendiente: corresponde al detalle del pedido
+		else
+			'x'
+	end
+from wp_posts wp inner join wp_wc_order_product_lookup wwopl
+		on wp.ID = wwopl.order_id 
+where post_status = 'wc-cancelled'
+
+-- wp.ID = 9487) -- as 'Dice_Contener'
 	  
 	  
 	  
 	  
 
-select count(*)
-from wp_postmeta 
-
-select *
-from wp_postmeta 
-where post_id = 8079 -- producto
-
-
-select *
-from wp_postmeta 
-where post_id = 9486 -- pedido
-
-select * from wp_users wu
-
-select * from wp_usermeta wu2 where user_id = 23
--- envia-webservice/webservice/envia-webservice.php
+++ envia-webservice/webservice/envia-webservice.php
@@ -6,12 +6,9 @@ include 'connection.php';
 // Obtener los pedidos que estén estado 'procesando' ('wc-processing') de la base de datos
 $sql = "SELECT *
         FROM wp_posts 
-        WHERE post_type = 'shop_order' AND post_status = 'wc-cancelled'; -- cambiar luego por wc-processing
-        -- WHERE post_type = 'shop_order' AND post_status = 'wc-processing'"; //cambiar luego por 
+        WHERE post_type = 'shop_order' AND post_status = 'wc-cancelled'"; // cambiar luego por wc-processing
 
 $resultQuery = $link->query($sql);
-// var_dump($resultQuery);
-
 
 // Formación de array de órdenes de pedidos que se deben incluir en la guía y que cumplen con el estado 'wc-processing'
 $arrayOrders = array();
@@ -19,46 +16,34 @@ $arrayOrders = array();
 while($row = $resultQuery->fetch_array()){
     $arrayOrders[] = $row['ID'];
 }
-echo "Órdenes canceladas\n";
-print_r($arrayOrders);
 
+
+// Extraer los datos necesarios de cada pedido para realizar la solicitud al Webservice de Envía y generar la guía
 $arrayData = array();
 $index = 0;
 $index2 = 0;
 
 foreach($arrayOrders as $order){
-    // Extraer los datos necesarios de cada pedido para realizar la solicitud al Webservice de Envía y generar la guía
     $sql =  "SELECT 
                 CONCAT(dcc.codigo_ciudad, ' - ', dcc.nombre_ciudad) AS 'Ciudad_Origen',
-                wpm.meta_value as 'Ciudad_Destino',
+                wpm.meta_value AS 'Ciudad_Destino', -- enviar código y organizar dato a capturar
                 '4' AS 'Cod_FormaPago',
                 '12' AS 'Cod_Servicio',
                 '1' AS 'Num_Unidades',
                 '1' AS 'MPesoReal_K',
                 '1' AS 'MPesoVolumen_K',
-                wpm.meta_value as 'Valor_Declarado',
+                TRUNCATE(wpm.meta_value, 0) AS 'Valor_Declarado', 
                 '0' AS 'Mca_DocInternacional',
                 '3' AS 'Cod_Regional_Cta',
                 '1' AS 'Cod_Oficina_Cta',
                 '03-001-0004582' AS 'Cod_Cuenta',
                 'SYAM COSMETICS SAS' AS 'Nom_Remitente',
                 'Carrera 54 # 79AA Sur - 40 Bodega 140 La Troja' AS 'Dir_Remitente',
-                '5963719' AS 'Tel_Remitente',
+                '3016883' AS 'Tel_Remitente',
                 '901040898-6' AS 'Ced_Remitente',
-                concat(wpm.meta_value, ' - ', wpm.meta_value) as 'Nom_Destinatario',
+                CONCAT(wpm.meta_value, ' - ', wpm.meta_value) AS 'Nom_Destinatario', -- organizar dato a capturar
                 $order AS 'Num_Documentos', -- Número de pedido para solicitar la generación de la guía,
-                /*(select 
-                    case wp.post_title
-                        when wp.post_title like '%KIT PIEL CANELA%'
-                            then concat('PG.', wwopl.product_qty, wwopl.product_id, 'kpc/')   -- Pendiente: corresponde al detalle del pedido
-                        when wp.post_title like '%CARBÓN ACTIVADO WIKI%' 
-                            then concat('PG.', wwopl.product_qty, wwopl.product_id, 'carb/')  -- Pendiente: corresponde al detalle del pedido
-                        else
-                            'x'
-                    end
-                from wp_posts wp
-                where wp.ID = '$order') AS 'Dice_Contener',*/
-                wp.post_excerpt as 'Texto_Guia',
+                wp.post_excerpt AS 'Texto_Guia',
                 '' AS 'Accion_NotaGuia',
                 '' AS 'Numero_Guia',
                 '' AS 'CentroCosto',
@@ -83,11 +68,10 @@ foreach($arrayOrders as $order){
              	wpm.meta_key = '_billing_last_name') AND
  	            -- wpm.meta_id = wpm2.meta_id AND
                 wpm.post_id = '$order'  
-                -- group by wpm.meta_id 
-                -- limit 1	
             ";
 
     $resultQuery = $link->query($sql);
+
 
     //Formación de array con el detalle del pedido para la generación de la guía
     while($row = $resultQuery->fetch_array()){
@@ -103,10 +87,10 @@ foreach($arrayOrders as $order){
         $arrayData[$index]['Cod_Regional_Cta'] = $row['Cod_Regional_Cta'];
         $arrayData[$index]['Cod_Oficina_Cta'] = $row['Cod_Oficina_Cta'];
         $arrayData[$index]['Cod_Cuenta'] = $row['Cod_Cuenta'];
-        $arrayData[$index]['Nom_Remitente'] = $row['Nom_Remitente'];
-        $arrayData[$index]['Dir_Remitente'] = $row['Dir_Remitente'];
-        $arrayData[$index]['Tel_Remitente'] = $row['Tel_Remitente'];
-        $arrayData[$index]['Ced_Remitente'] = $row['Ced_Remitente'];
+        $arrayData[$index]['info_origen']['Nom_Remitente'] = $row['Nom_Remitente'];
+        $arrayData[$index]['info_origen']['Dir_Remitente'] = $row['Dir_Remitente'];
+        $arrayData[$index]['info_origen']['Tel_Remitente'] = $row['Tel_Remitente'];
+        $arrayData[$index]['info_origen']['Ced_Remitente'] = $row['Ced_Remitente'];
         $arrayData[$index]['Nom_Destinatario'] = $row['Nom_Destinatario'];
         $arrayData[$index]['Num_Documentos'] = $row['Num_Documentos'];
         $arrayData[$index]['Texto_Guia'] = $row['Texto_Guia'];
@@ -118,30 +102,33 @@ foreach($arrayOrders as $order){
         $arrayData[$index]['valorproducto'] = $row['valorproducto'];
     }
 
+
     // Extraer el detalle de la orden de pedido
     $sql2 = "SELECT *
             FROM wp_posts wp INNER JOIN wp_wc_order_product_lookup wwopl 
                 ON wp.ID = wwopl.product_id 
             WHERE wwopl.order_id = '$order'";
-    $resultQuery2 = $link->query($sql2);
 
+    $resultQuery2 = $link->query($sql2);
     $strSaysContaint = "PG.";
     $index2 = 0;
+    $arrayDetails = array();
 
     while($row2 = $resultQuery2->fetch_array()){
-        $arrayDetails[$index2]['product_id'] = $row2['product_id'];
         $arrayDetails[$index2]['post_title'] = $row2['post_title'];
         $arrayDetails[$index2]['product_qty'] = $row2['product_qty'];
-        $arrayDetails[$index2]['order_id'] = $row2['order_id'];
         $strSaysContaint .= $arrayDetails[$index2]['product_qty'] . $arrayDetails[$index2]['post_title'] . "/";
         ++$index2;
     }
 
-    $arrayData[$index]['Dice_Contener'] = $strSaysContaint;
+    $arrayData[$index]['Dice_Contener'] = $strSaysContaint; // Organizar para que sume cantidades del mismo producto y muestre solo una descripción de ésta
     ++$index;
 }
 
 // echo "\nDetalle orden\n";
-print_r($arrayData);
+// print_r($arrayData);
 
 $link->close();
+
+echo json_encode($arrayData);
+
